43a33e5b4782841685fba0e646fcd72a9845308d
Akbar Hamaminatu <me@akbr.dev>
2021-04-27 04:58:11 +0700

    feat(middleware): Add isAdmin middleware

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::user() &&  Auth::user()->roles == 'admin') {
+            return $next($request);
+        }
+        abort(403);
+    }
+}
