310e0d5c5276b141cce5968deef21c0e87052b81
Akbar Hamaminatu <me@akbr.dev>
2021-04-27 09:54:04 +0700

    feat(AppServiceProvider): Add isAdmin blades functiom

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Blade::directive('isAdmin', function() {
+            return "<?php if(Auth::user()->roles=='admin'): ?>";
+        });
+
+        Blade::directive('endisAdmin', function() {
+            return "<?php endif; ?>";
+        });
     }
 }
