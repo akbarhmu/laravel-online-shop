fa0150ea92067264f345115359bf1fc4eb023c7a
Akbar Hamaminatu <me@akbr.dev>
2021-04-27 12:28:45 +0700

    feat(AppServiceProvider): Use bootstrap for pagination

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
@@ -24,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Paginator::useBootstrap();
+
         Blade::directive('isAdmin', function() {
             return "<?php if(Auth::user()->roles=='admin'): ?>";
         });
