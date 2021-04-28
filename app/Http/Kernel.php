30ead2d137966f749c9fb15898cdda38416a86a4
Akbar Hamaminatu <me@akbr.dev>
2021-04-28 22:58:58 +0700

    feat(middleware): Add is admin middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -53,6 +53,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
+        'admin' => \Illuminate\Auth\Middleware\Admin::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
