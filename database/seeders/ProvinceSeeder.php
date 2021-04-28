a06665956dae3d025f37a3f7f5ecf9c38cc374d2
Akbar Hamaminatu <me@akbr.dev>
2021-04-28 22:11:46 +0700

    feat(database): Add province resources

---
+++ database/seeders/ProvinceSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Province;
+use Illuminate\Database\Seeder;
+use Kavist\RajaOngkir\Facades\RajaOngkir;
+
+class ProvinceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $provinces = RajaOngkir::provinsi()->all();
+        foreach($provinces as $province){
+            Province::create([
+                'province' => $province['province'],
+            ]);
+        }
+    }
+}
