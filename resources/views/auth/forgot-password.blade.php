e54619e72923ee9df9abf73446a07bfb32c52be4
Akbar Hamaminatu <me@akbr.dev>
2021-05-03 07:54:04 +0700

    feat(views): Adapt forgot password page with new templates

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,37 +1,52 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
-
-        <div class="card-body">
-
-            <div class="mb-3">
-                {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+@section('title', __('Forgot Password'))
+@extends('layouts.guest')
+@section('content')
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+            <div class="login-brand">
+                <x-jet-authentication-card-logo />
             </div>
+            <div class="card card-primary">
+                <div class="card-header"><h4>{{__('Forgot Password')}}</h4></div>
 
-            @if (session('status'))
-                <div class="alert alert-success" role="alert">
-                    {{ session('status') }}
+                <div class="card-body">
+
+                    <div class="alert alert-primary alert-has-icon">
+                        <div class="alert-icon"><i class="far fa-lightbulb"></i></div>
+                        <div class="alert-body text-justify">
+                            <div class="alert-title">{{__('Forgot your password?')}}</div>
+                            {{__('No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.')}}
+                        </div>
+                    </div>
+
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <form method="POST" action="/forgot-password" class="needs-validation">
+                        @csrf
+
+                        <div class="form-group">
+                            <x-jet-label value="{{__('Email')}}" />
+                            <x-jet-input type="email" name="email" :value="old('email')" required autofocus />
+                            <x-jet-input-error class="invalid-feedback" for="email"></x-jet-input-error>
+                        </div>
+
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                                {{ __('Email Password Reset Link') }}
+                            </button>
+                        </div>
+                    </form>
                 </div>
-            @endif
-
-            <x-jet-validation-errors class="mb-3" />
-
-            <form method="POST" action="/forgot-password">
-                @csrf
-
-                <div class="form-group">
-                    <x-jet-label value="Email" />
-                    <x-jet-input type="email" name="email" :value="old('email')" required autofocus />
-                </div>
-
-                <div class="d-flex justify-content-end mt-4">
-                    <x-jet-button>
-                        {{ __('Email Password Reset Link') }}
-                    </x-jet-button>
-                </div>
-            </form>
+            </div>
+            <div class="simple-footer">
+                <x-copyright />
+            </div>
         </div>
-    </x-jet-authentication-card>
-</x-guest-layout>
+    </div>
+</div>
+@endsection
