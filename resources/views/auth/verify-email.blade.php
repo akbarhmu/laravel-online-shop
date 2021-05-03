19e547d6451f03ebbffd44e50b058eb39a51b2d0
Akbar Hamaminatu <me@akbr.dev>
2021-05-03 07:54:55 +0700

    feat(views): Adapt verify email page with new templates

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,39 +1,55 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
-
-        <div class="card-body">
-            <div class="mb-3 small text-muted">
-                {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+@section('title', __('Verify Email Address'))
+@extends('layouts.guest')
+@section('content')
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+            <div class="login-brand">
+                <x-jet-authentication-card-logo />
             </div>
-
-            @if (session('status') == 'verification-link-sent')
-                <div class="alert alert-success" role="alert">
-                    {{ __('A new verification link has been sent to the email address you provided during registration.') }}
-                </div>
-            @endif
-
-            <div class="mt-4 d-flex justify-content-between">
-                <form method="POST" action="{{ route('verification.send') }}">
-                    @csrf
-
-                    <div>
-                        <x-jet-button type="submit">
-                            {{ __('Resend Verification Email') }}
-                        </x-jet-button>
+            <div class="card card-primary">
+                <div class="card-header"><h4>{{__('Verify Email Address')}}</h4></div>
+                <div class="card-body">
+                    <div class="alert alert-primary alert-has-icon">
+                        <div class="alert-icon"><i class="far fa-lightbulb"></i></div>
+                        <div class="alert-body text-justify">
+                            <div class="alert-title">{{__('Verify Email Address')}}</div>
+                            {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+                        </div>
                     </div>
-                </form>
 
-                <form method="POST" action="/logout">
-                    @csrf
+                    @if (session('status') == 'verification-link-sent')
+                        <div class="alert alert-success" role="alert">
+                            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+                        </div>
+                    @endif
 
-                    <button type="submit" class="btn btn-link">
-                        {{ __('Log Out') }}
-                    </button>
-                </form>
+                    <div class="mt-4 d-flex justify-content-between">
+                        <form method="POST" action="{{ route('verification.send') }}">
+                            @csrf
+
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-dark" tabindex="4">
+                                    {{ __('Resend Verification Email') }}
+                                </button>
+                            </div>
+                        </form>
+
+                        <form method="POST" action="/logout">
+                            @csrf
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-link">
+                                    {{ __('Log Out') }}
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="simple-footer">
+                <x-copyright />
             </div>
         </div>
-    </x-jet-authentication-card>
-</x-guest-layout>
+    </div>
+</div>
+@endsection
