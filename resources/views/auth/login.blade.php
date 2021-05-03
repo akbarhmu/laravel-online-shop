ae5c48321c1685f540d6a91a0d6ee72eb304ce3c
Akbar Hamaminatu <me@akbr.dev>
2021-05-03 07:54:20 +0700

    feat(views): Adapt login page with new templates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,60 +1,73 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+@section('title', __('Log in'))
+@extends('layouts.guest')
+@section('content')
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+            <div class="login-brand">
+                <x-jet-authentication-card-logo />
+            </div>
+            <div class="card card-primary">
+                <div class="card-header"><h4>{{__('Log in')}}</h4></div>
+                <div class="card-body">
 
-        <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success mb-3 rounded-0" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
 
-            <x-jet-validation-errors class="mb-3 rounded-0" />
+                    <form method="POST" action="{{ route('login') }}" class="needs-validation">
+                        @csrf
+                        <div class="form-group">
+                            <x-jet-label value="{{ __('Email') }}" />
 
-            @if (session('status'))
-                <div class="alert alert-success mb-3 rounded-0" role="alert">
-                    {{ session('status') }}
+                            <x-jet-input class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" type="email"
+                                        name="email" :value="old('email')" autocomplete="email" required autofocus />
+                            <x-jet-input-error class="invalid-feedback" for="email"></x-jet-input-error>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="d-block">
+                                <label for="password" class="control-label">{{ __('Password') }}</label>
+                                @if (Route::has('password.request'))
+                                    <div class="float-right">
+                                        <a href="{{ route('password.request') }}" class="text-small">
+                                            {{__('Forgot Password?')}}
+                                        </a>
+                                    </div>
+                                @endif
+                            </div>
+                            <x-jet-input class="form-control {{ $errors->has('password') ? ' is-invalid' : '' }}" type="password"
+                                        name="password" required autocomplete="current-password" />
+                            <x-jet-input-error class="invalid-feedback" for="password"></x-jet-input-error>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="custom-control custom-checkbox">
+                                <x-jet-checkbox class="custom-control-input" id="remember_me" name="remember" />
+                                <label class="custom-control-label" for="remember_me">
+                                    {{ __('Remember Me') }}
+                                </label>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                                {{__('Log in')}}
+                            </button>
+
+                            <div class="mt-4 text-muted text-center">
+                                {{__("Don't have an account?")}} <a href="{{route('register')}}">{{__('Register')}}</a>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-            @endif
-
-            <form method="POST" action="{{ route('login') }}">
-                @csrf
-                <div class="form-group">
-                    <x-jet-label value="{{ __('Email') }}" />
-
-                    <x-jet-input class="{{ $errors->has('email') ? 'is-invalid' : '' }}" type="email"
-                                 name="email" :value="old('email')" required />
-                    <x-jet-input-error for="email"></x-jet-input-error>
-                </div>
-
-                <div class="form-group">
-                    <x-jet-label value="{{ __('Password') }}" />
-
-                    <x-jet-input class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" type="password"
-                                 name="password" required autocomplete="current-password" />
-                    <x-jet-input-error for="password"></x-jet-input-error>
-                </div>
-
-                <div class="form-group">
-                    <div class="custom-control custom-checkbox">
-                        <x-jet-checkbox id="remember_me" name="remember" />
-                        <label class="custom-control-label" for="remember_me">
-                            {{ __('Remember Me') }}
-                        </label>
-                    </div>
-                </div>
-
-                <div class="mb-0">
-                    <div class="d-flex justify-content-end align-items-baseline">
-                        @if (Route::has('password.request'))
-                            <a class="text-muted mr-3" href="{{ route('password.request') }}">
-                                {{ __('Forgot your password?') }}
-                            </a>
-                        @endif
-
-                        <x-jet-button>
-                            {{ __('Log in') }}
-                        </x-jet-button>
-                    </div>
-                </div>
-            </form>
+            </div>
+            <div class="simple-footer">
+                <x-copyright />
+            </div>
         </div>
-    </x-jet-authentication-card>
-</x-guest-layout>
+    </div>
+</div>
+@endsection
