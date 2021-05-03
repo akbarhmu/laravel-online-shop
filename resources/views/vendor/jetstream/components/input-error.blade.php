9c2af0ee3dbb63a730c779585c82cb1df7b2e49e
Akbar Hamaminatu <me@akbr.dev>
2021-05-03 07:55:29 +0700

    fix(components): Fix error message not displaying

--- resources/views/vendor/jetstream/components/input-error.blade.php
+++ resources/views/vendor/jetstream/components/input-error.blade.php
@@ -1,7 +1,7 @@
 @props(['for'])
 
 @error($for)
-    <span {{ $attributes->merge(['class' => 'invalid-feedback']) }} role="alert">
+    <span {{ $attributes->merge(['class' => 'invalid-feedback d-block']) }} role="alert">
         <strong>{{ $message }}</strong>
     </span>
 @enderror
