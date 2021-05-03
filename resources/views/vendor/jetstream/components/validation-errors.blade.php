7e27a8f4977188a95d6c7aff8cd291257fc63c0d
Akbar Hamaminatu <me@akbr.dev>
2021-05-03 07:56:26 +0700

    feat(components): Adapt validation error components with new templates

--- resources/views/vendor/jetstream/components/validation-errors.blade.php
+++ resources/views/vendor/jetstream/components/validation-errors.blade.php
@@ -1,11 +1,10 @@
 @if ($errors->any())
-    <div {!! $attributes->merge(['class' => 'alert alert-danger text-sm p-2']) !!} role="alert">
-        <div class="font-weight-bold">{{ __('Whoops! Something went wrong.') }}</div>
-
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
+    <div {!! $attributes->merge(['class' => 'alert alert-danger']) !!} role="alert">
+        <div class="alert-title font-weight-bold">{{ __('Whoops! Something went wrong.') }}</div>
+        <ul class="">
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
         </ul>
     </div>
 @endif
