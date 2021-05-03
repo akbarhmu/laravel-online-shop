39335fa31916aac5ba73e2570dcb13f636b3598d
Akbar Hamaminatu <me@akbr.dev>
2021-05-03 13:22:45 +0700

    style: Add stisla template resources

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .js('resources/js/page/index.js', 'public/js/page/index.js')
     .sass('resources/sass/app.scss', 'public/css')
     .webpackConfig(require('./webpack.config'));
 
