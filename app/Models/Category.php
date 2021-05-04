c39edfd01840a0542ff7f403da401ad707b1dfd9
Akbar Hamaminatu <me@akbr.dev>
2021-05-04 11:44:29 +0700

    fix(models): Fix category relations

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -15,6 +15,6 @@ class Category extends Model
 
     public function products()
     {
-        return $this->hasMany(Product::class, 'category_id');
+        return $this->hasMany(Product::class);
     }
 }
