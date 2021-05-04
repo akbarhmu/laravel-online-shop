a0a685598fdfa18ff88fdc45fb0e343f1759d01e
Akbar Hamaminatu <me@akbr.dev>
2021-05-04 11:44:05 +0700

    feat(views): Move alert auto dismiss function to child views

--- resources/views/dashboard/category/index.blade.php
+++ resources/views/dashboard/category/index.blade.php
@@ -94,5 +94,12 @@
             }
         });
     });
+    $(document).ready(function(){
+        window.setTimeout(function() {
+            $(".alert").fadeTo(500, 0).slideUp(500, function(){
+            $(this).remove();
+            });
+        }, 5000);
+    });
 </script>
 @endsection
