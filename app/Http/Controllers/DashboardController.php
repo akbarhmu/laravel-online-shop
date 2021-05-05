fef865866be45b91e04e87980320193d733bcf88
Akbar Hamaminatu <me@akbr.dev>
2021-05-05 16:26:26 +0700

    feat(views): Redesign dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -13,7 +14,8 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('dashboard.index');
+        $customersCount = User::where('isAdmin', false)->count();
+        return view('dashboard.index', ['customersCount'=>$customersCount]);
     }
 
     /**
