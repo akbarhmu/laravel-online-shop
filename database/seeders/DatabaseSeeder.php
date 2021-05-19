fe43fad4f88b862f25c2a8ea2ddd3e3a4979c876
Akbar Hamaminatu <me@akbr.dev>
2021-05-19 09:29:16 +0700

    feat(seeders): Include ShopSeeder in database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             ProvinceSeeder::class,
             CitySeeder::class,
             UserSeeder::class,
+            ShopSeeder::class,
         ]);
     }
 }
