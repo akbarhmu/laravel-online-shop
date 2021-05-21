f6b1987317e7e3af04b88143e85275b5b066ed9c
Akbar Hamaminatu <me@akbr.dev>
2021-05-21 21:29:29 +0700

    feat(views): Redesign front page

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>ELECTROPARADIZO</title>
+        <title>{{$site_title}}</title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Font Awesome icons (free version)-->
@@ -14,28 +14,31 @@
         <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
         <!-- Third party plugin CSS-->
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+        <link href="{{asset('css/user/magnific-popup.css')}}" rel="stylesheet" />
         <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="{{ asset('css/style_landing_purple.css') }}" rel="stylesheet" />
+        <link href="{{asset('/css/user/style_landing_purple.css')}}" rel="stylesheet" />
+        <link rel="stylesheet" href="{{asset('css/user/owl.carousel.min.css')}}">
+        <link rel="stylesheet" href="{{asset('css/user/owl.theme.default.min.css')}}">
+        <link rel="stylesheet" href="{{asset('css/user/app.css')}}" />
     </head>
     <body id="page-top">
         <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="secondNav">
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
             <div class="container">
-                <a class="navbar-brand js-scroll-trigger" href="#page-top">ELECTROPARADIZO</a>
+                <a class="navbar-brand js-scroll-trigger" href="#page-top">{{$site_title}}</a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ml-auto my-2 my-lg-0">
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#services">Services</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">Contact</a></li>
+                        <li class="nav-item"><a class="nav-link {{Custom::set_active(['/'])}} js-scroll-trigger" href="{{route('index')}}">{{__('Home')}}</a></li>
+                        <li class="nav-item"><a class="nav-link {{Custom::set_active(['product*'])}} js-scroll-trigger" href="{{route('user.products.index')}}">{{__('Products')}}</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#">{{__('Services')}}</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#">{{__('Contact')}}</a></li>
                     </ul>
                 </div>
             </div>
         </nav>
         <!-- Masthead-->
-        {{-- <header class="masthead">
+        <header class="masthead">
             <div class="container h-100">
                 <div class="row h-100 align-items-center justify-content-center text-center">
                     <div class="col-lg-10 align-self-end">
@@ -43,31 +46,31 @@
                         <hr class="divider my-4" />
                     </div>
                     <div class="col-lg-8 align-self-baseline">
-                        <p class="text-white-75 font-weight-light mb-0">ELECTROPARADIZO menyediakan berbagai macam produk elektronik baru maupun bekas</p>
+                        <p class="text-white-75 font-weight-light mb-0">{{$site_title}} menyediakan berbagai macam produk elektronik baru maupun bekas</p>
                         <p class="text-white-75 font-weight-light mb-5">Kami juga menyediakan layanan service produk elektronis mulai dari TV, monitor, komputer, kulkas dan lain-lain</p>
-                        <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">LOGIN</a>
-                        <a class="btn btn-primary btn-xl js-scroll-trigger ml-3" href="#about">REGISTER</a>
+                        <a class="btn btn-primary btn-xl js-scroll-trigger" href="{{route('user.products.index')}}"><i class="fas fa-shopping-cart"></i> {{__('Products')}}</a>
+                        <a class="btn btn-primary btn-xl js-scroll-trigger ml-3" href="#"><i class="fas fa-tools"></i> {{__('Service')}}</a>
                     </div>
                 </div>
             </div>
-        </header> --}}
+        </header>
         <!-- About-->
-        {{-- <section class="page-section bg-primary" id="about">
+        <section class="page-section bg-primary" id="about">
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-lg-8 text-center">
                         <h2 class="text-white mt-0">Belanja dan Service Serasa di Surga!</h2>
                         <hr class="divider light my-4" />
-                        <p class="text-white-50 mb-4">ELECTROPARADIZO menyediakan berbagai macam produk dan terdapat layanan servis produk elektronik! Hanya dengan daftar dan nikmati berbagai fitur yang ada di web ini!</p>
-                        <a class="btn btn-light btn-xl js-scroll-trigger" href="#services">Apa saja di ELECTROPARADIZO?</a>
+                        <p class="text-white-50 mb-4">{{$site_title}} menyediakan berbagai macam produk dan terdapat layanan servis produk elektronik! Hanya dengan daftar dan nikmati berbagai fitur yang ada di web ini!</p>
+                        <a class="btn btn-light btn-xl js-scroll-trigger" href="#services">Apa saja di {{$site_title}}?</a>
                     </div>
                 </div>
             </div>
-        </section> --}}
+        </section>
         <!-- Services-->
         <section class="page-section" id="services">
             <div class="container">
-                <h2 class="text-center mt-0">Tentang ELECTROPARADIZO</h2>
+                <h2 class="text-center mt-0">Tentang {{$site_title}}</h2>
                 <hr class="divider my-4" />
                 <div class="row">
                     <div class="col-lg-3 col-md-6 text-center">
@@ -101,72 +104,32 @@
                 </div>
             </div>
         </section>
-        <!-- Portfolio-->
-        <div id="portfolio">
-            <div class="container-fluid p-0">
-                <div class="row no-gutters">
-                    <div class="col-lg-4 col-sm-6">
-                        <a class="portfolio-box" href="{{ asset('img/portfolio/fullsize/1.jpg') }}">
-                            <img class="img-fluid" src="{{ asset('img/portfolio/thumbnails/1.jpg') }}" alt="..." />
-                            <div class="portfolio-box-caption">
-                                <div class="project-category text-white-50">Category</div>
-                                <div class="project-name">Project Name</div>
+
+        <!-- Products-->
+        <section class="page-section" id="products">
+            <div class="container">
+                <h2 class="text-center mt-0">Produk Unggulan</h2>
+                <hr class="divider my-4" />
+                <div class="row nonloop-block-3 owl-carousel">
+                        @forelse ($products as $product)
+                        <div class="item">
+                            <div class="block-4 text-center">
+                                <a href="{{route('products.show', $product->id)}}">
+                                    <figure class="block-4-image">
+                                        <img src="{{asset('storage/'.$product->image)}}" alt="Image placeholder" class="img-fluid">
+                                    </figure>
+                                    <div class="block-4-text p-4">
+                                        <h3><a href="#">{{$product->name}}</a></h3>
+                                        <p class="badge badge-dark mb-0">{{$product->categories->name}}</p>
+                                        <p class="text-primary font-weight-bold">@rupiah($product->price)</p>
+                                    </div>
+                                </a>
                             </div>
-                        </a>
-                    </div>
-                    <div class="col-lg-4 col-sm-6">
-                        <a class="portfolio-box" href="{{ asset('img/portfolio/fullsize/2.jpg') }}">
-                            <img class="img-fluid" src="{{ asset('img/portfolio/thumbnails/2.jpg') }}" alt="..." />
-                            <div class="portfolio-box-caption">
-                                <div class="project-category text-white-50">Category</div>
-                                <div class="project-name">Project Name</div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-lg-4 col-sm-6">
-                        <a class="portfolio-box" href="{{ asset('img/portfolio/fullsize/3.jpg') }}">
-                            <img class="img-fluid" src="{{ asset('img/portfolio/thumbnails/3.jpg') }}" alt="..." />
-                            <div class="portfolio-box-caption">
-                                <div class="project-category text-white-50">Category</div>
-                                <div class="project-name">Project Name</div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-lg-4 col-sm-6">
-                        <a class="portfolio-box" href="{{ asset('img/portfolio/fullsize/4.jpg') }}">
-                            <img class="img-fluid" src="{{ asset('img/portfolio/thumbnails/4.jpg') }}" alt="..." />
-                            <div class="portfolio-box-caption">
-                                <div class="project-category text-white-50">Category</div>
-                                <div class="project-name">Project Name</div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-lg-4 col-sm-6">
-                        <a class="portfolio-box" href="{{ asset('img/portfolio/fullsize/5.jpg') }}">
-                            <img class="img-fluid" src="{{ asset('img/portfolio/thumbnails/5.jpg') }}" alt="..." />
-                            <div class="portfolio-box-caption">
-                                <div class="project-category text-white-50">Category</div>
-                                <div class="project-name">Project Name</div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-lg-4 col-sm-6">
-                        <a class="portfolio-box" href="{{ asset('img/portfolio/fullsize/6.jpg') }}">
-                            <img class="img-fluid" src="{{ asset('img/portfolio/thumbnails/6.jpg') }}" alt="..." />
-                            <div class="portfolio-box-caption p-3">
-                                <div class="project-category text-white-50">Category</div>
-                                <div class="project-name">Project Name</div>
-                            </div>
-                        </a>
-                    </div>
+                        </div>
+                    @empty
+
+                    @endforelse
                 </div>
-            </div>
-        </div>
-        <!-- Call to action-->
-        <section class="page-section bg-dark text-white">
-            <div class="container text-center">
-                <h2 class="mb-4">Free Download at Start Bootstrap!</h2>
-                <a class="btn btn-light btn-xl" href="https://startbootstrap.com/theme/creative/">Download Now!</a>
             </div>
         </section>
 
@@ -183,7 +146,7 @@
                 <div class="row">
                     <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
                         <i class="fas fa-phone fa-3x mb-3 text-muted"></i>
-                        <div>+6285784561260</div>
+                        <a class="d-block" href="telp:{{$site_phone}}">{{$site_phone}}</a>
                     </div>
                     <div class="col-lg-4 mr-auto text-center">
                         <i class="fas fa-envelope fa-3x mb-3 text-muted"></i>
@@ -198,12 +161,10 @@
             <div class="container">
                 <div class="small text-center text-muted">
                     Copyright &copy;
-                    <!-- This script automatically adds the current year to your website footer-->
-                    <!-- (credit: https://updateyourfooter.com/)-->
                     <script>
                         document.write(new Date().getFullYear());
                     </script>
-                    - ELECTROPARADIZO Team
+                    - {{$site_title}} Team
                 </div>
             </div>
         </footer>
@@ -213,91 +174,7 @@
         <!-- Third party plugin JS-->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
-        <!-- Core theme JS-->
-        <script>
-            (function ($) {
-                "use strict"; // Start of use strict
-
-                // Smooth scrolling using anime.js
-                $('a.js-scroll-trigger[href*="#"]:not([href="#"])').on('click', function () {
-                    if (
-                        location.pathname.replace(/^\//, "") ==
-                        this.pathname.replace(/^\//, "") &&
-                        location.hostname == this.hostname
-                    ) {
-                        var target = $(this.hash);
-                        target = target.length ?
-                            target :
-                            $("[name=" + this.hash.slice(1) + "]");
-                        if (target.length) {
-                            anime({
-                                targets: 'html, body',
-                                scrollTop: target.offset().top - 72,
-                                duration: 1000,
-                                easing: 'easeInOutExpo'
-                            });
-                            return false;
-                        }
-                    }
-                });
-
-                // Closes responsive menu when a scroll trigger link is clicked
-                $('.js-scroll-trigger').click(function () {
-                    $('.navbar-collapse').collapse('hide');
-                });
-
-                // Activate scrollspy to add active class to navbar items on scroll
-                $('body').scrollspy({
-                    target: '#secondNav',
-                    offset: 75
-                });
-
-                // Collapse Navbar
-                var navbarCollapse = function () {
-                    if ($("#secondNav").offset().top > 100) {
-                        $("#secondNav").addClass("navbar-scrolled");
-                    }
-                    else {
-                        $("#secondNav").removeClass("navbar-scrolled");
-                    }
-                };
-
-                // var navbarSecondCollapse = function () {
-                //     if ($("#secondNav").offset().top > 100) {
-                //         $("#secondNav").addClass("navbar-scrolled");
-                //     }
-                //     else {
-                //         $("#secondNav").removeClass("navbar-scrolled");
-                //     }
-                // };
-
-                // Collapse now if page is not at top
-                // navbarCollapse();
-                navbarCollapse();
-                // navbarSecondCollapse();
-
-                // Collapse the navbar when page is scrolled
-                $(window).scroll(navbarCollapse);
-                // $(window).scroll(navbarSecondCollapse);
-
-
-                // Magnific popup calls
-                $('#portfolio').magnificPopup({
-                    delegate: 'a',
-                    type: 'image',
-                    tLoading: 'Loading image #%curr%...',
-                    mainClass: 'mfp-img-mobile',
-                    gallery: {
-                        enabled: true,
-                        navigateByImgClick: true,
-                        preload: [0, 1]
-                    },
-                    image: {
-                        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-                    }
-                });
-
-            })(jQuery); // End of use strict
-        </script>
+        <script src="{{asset('js/user/scripts_landing.js')}}"></script>
+        <script src="{{asset('js/user/owl.carousel.min.js')}}"></script>
     </body>
 </html>
