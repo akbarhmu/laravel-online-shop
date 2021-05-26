668ae384282c378a34b760aff608018ca96fce73
Akbar Hamaminatu <me@akbr.dev>
2021-05-26 19:56:40 +0700

    feat(models): Add services table model

---
+++ app/Models/Service.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Service extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'address',
+        'phone',
+        'product_name',
+        'product_merk',
+        'keluhan',
+        'image',
+        'status'
+    ];
+}
