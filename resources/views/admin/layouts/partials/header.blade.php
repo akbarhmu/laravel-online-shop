acc814b0eb31ee0c2646f9d4a5667a326a8e526d
Akbar Hamaminatu <me@akbr.dev>
2021-05-27 19:46:13 +0700

    feat(views): Remove unused elements

--- resources/views/admin/layouts/partials/header.blade.php
+++ resources/views/admin/layouts/partials/header.blade.php
@@ -6,51 +6,6 @@
     </ul>
     </form>
     <ul class="navbar-nav navbar-right">
-    <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class="nav-link nav-link-lg message-toggle beep"><i class="far fa-envelope"></i></a>
-        <div class="dropdown-menu dropdown-list dropdown-menu-right">
-        <div class="dropdown-header">Messages
-            <div class="float-right">
-            <a href="#">Mark All As Read</a>
-            </div>
-        </div>
-        <div class="dropdown-list-content dropdown-list-message">
-            <a href="#" class="dropdown-item dropdown-item-unread">
-                <div class="dropdown-item-avatar">
-                    <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle">
-                    <div class="is-online"></div>
-                </div>
-                <div class="dropdown-item-desc">
-                    <b>Kusnaedi</b>
-                    <p>Hello, Bro!</p>
-                    <div class="time">10 Hours Ago</div>
-                </div>
-            </a>
-        </div>
-        <div class="dropdown-footer text-center">
-            <a href="#">View All <i class="fas fa-chevron-right"></i></a>
-        </div>
-        </div>
-    </li>
-    <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class="nav-link notification-toggle nav-link-lg beep"><i class="far fa-bell"></i></a>
-        <div class="dropdown-menu dropdown-list dropdown-menu-right">
-        <div class="dropdown-header">Notifications
-        </div>
-        <div class="dropdown-list-content dropdown-list-icons">
-            <a href="#" class="dropdown-item dropdown-item-unread">
-                <div class="dropdown-item-icon bg-primary text-white">
-                    <i class="fas fa-code"></i>
-                </div>
-                <div class="dropdown-item-desc">
-                    Template update is available now!
-                    <div class="time text-primary">2 Min Ago</div>
-                </div>
-            </a>
-        </div>
-        <div class="dropdown-footer text-center">
-            <a href="#">View All <i class="fas fa-chevron-right"></i></a>
-        </div>
-        </div>
-    </li>
     <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
         @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
             <img alt="image" width="30" height="30" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" class="rounded-circle mr-1">
