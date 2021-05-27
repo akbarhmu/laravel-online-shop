26359ccb5abe5a1063c63ff9648693adb63cdda5
Akbar Hamaminatu <me@akbr.dev>
2021-05-27 09:32:48 +0700

    feat(controllers): Remove unused imports

--- app/Http/Controllers/user/PageController.php
+++ app/Http/Controllers/user/PageController.php
@@ -3,15 +3,9 @@
 namespace App\Http\Controllers\user;
 
 use App\Http\Controllers\Controller;
-use App\Models\Cart;
 use App\Models\Category;
 use App\Models\Product;
-use App\Models\Province;
-use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\View;
 
 class PageController extends Controller
 {
