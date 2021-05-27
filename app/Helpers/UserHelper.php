752e898db98e489b99fd4325d5b269294bfe1f7d
Akbar Hamaminatu <me@akbr.dev>
2021-05-27 09:31:38 +0700

    feat(helpers): Add function to get cart count

--- app/Helpers/UserHelper.php
+++ app/Helpers/UserHelper.php
@@ -1,11 +1,16 @@
 <?php
 namespace App\Helpers;
 
+use App\Models\Cart;
+
 class User {
     public static function set_active($path, $active = 'active') {
         return call_user_func_array('Request::is', (array)$path) ? $active : '';
     }
     public static function set_menu($path) {
         return call_user_func_array('Request::is', (array)$path) ? 'btn btn-primary text-light d-block d-sm-inline-block' : 'btn btn-secondary d-block text-dark d-sm-inline-block';
+    }
+    public static function getCartCount($id) {
+        return Cart::where('user_id', $id)->count();
     }
 }
