46bef6e48b0262b5fda2101bdbb6413b1167c925
Akbar Hamaminatu <me@akbr.dev>
2021-05-27 09:39:07 +0700

    feat(views): Update translations

--- resources/views/user/order/success.blade.php
+++ resources/views/user/order/success.blade.php
@@ -6,9 +6,9 @@
         <div class="row p-2">
             <div class="col-md-12 text-center">
                 <span class="icon-check_circle display-3 text-success"></span>
-                <h2 class="display-3 text-black" style="font-size: 4rem !important;">Terimakasih!</h2>
+                <h2 class="display-3 text-black" style="font-size: 4rem !important;">{{__('Thank you')}}!</h2>
                 <p class="lead mb-5">Pesanan Kamu Berhasil Dibuat Dengan No Invoice <a class="font-weight-bold" href="{{route('orders.show', $order->invoice)}}">{{$order->invoice}}</a>.</p>
-                <p><a href="{{route('orders.payment', $order->invoice)}}" class="btn btn-sm btn-primary">Pembayaran</a></p>
+                <p><a href="{{route('orders.payment', $order->invoice)}}" class="btn btn-sm btn-primary">{{__('Payment')}}</a></p>
             </div>
         </div>
     </div>
